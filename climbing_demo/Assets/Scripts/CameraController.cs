630936c111520f729c2aea2444903d67c1f1b3e2
Pedro Lucas <pedro.lucas.bravo@gmail.com>
2019-11-26 23:25:50 -0500

    Modify unity 2d demo in order to use with tap an accelerometer.

---
+++ climbing_demo/Assets/Scripts/CameraController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform playerTrans;
+    public float accelerometerFactor;
+    public float smoothTime;
+
+    private void Awake() {
+        trans_ = transform;
+    }
+
+    private void LateUpdate() {
+        var pos = playerTrans.position;
+        pos.z = trans_.position.z;
+        trans_.position = pos;
+        float velocity = 0;
+        var angle = Mathf.SmoothDampAngle(trans_.rotation.eulerAngles.z, Input.acceleration.x * accelerometerFactor, ref velocity, smoothTime);
+        trans_.rotation = Quaternion.Euler(0, 0, angle);
+    }
+
+    Transform trans_;
+}
